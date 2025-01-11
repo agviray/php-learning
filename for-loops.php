4c72f849e7665241e27ff80f7db079bc6d3aba11
Allan Viray <allang.v.dev@gmail.com>
2025-01-11 17:42:47 -0600

    Edit for-loops.php file

--- for-loops.php
+++ for-loops.php
@@ -4,13 +4,13 @@
   // =================
   // *** Same usage as in JavaScript ***
   // A for loop in PHP will repeat some code a certain number of times.
-
+  /*
   for ($i = 10; $i >= 0; $i -= 2) {
     // echo "Hello <br>";
     // echo $i . "<br>";
     echo $i . "<br>";
   }
-
+*/
   // The code below is a note for browser display.
   echo "
   <br> ========================================== <br>
@@ -28,17 +28,15 @@
 </head>
 <body>
   <form action="for-loops.php" method="post">
-    <label>Enter a number to count to:</label>
-    <input type="text" name="counter">
-    <input type="submit" value="Start">
+    <label>Enter a number to count DOWN to:</label>
+    <input type="text" name="count-down-counter">
+    <input type="submit" value="Start counting down">
   </form>
 </body>
 </html>
 
 <?php
-  $counter = $_POST["counter"];
-  
-  for ($i = 0; $i <= $counter; $i++) {
+  for ($i = $countDownCounter; $i >= 0; $i--) {
     echo $i . "<br>";
   }
 ?>
