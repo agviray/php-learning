65f4b426ded63b11a1b92ba6302d3a3b1c948c7b
Allan Viray <allang.v.dev@gmail.com>
2025-01-11 17:50:59 -0600

    Create while-loops.php file and content

---
+++ while-loops.php
@@ -0,0 +1,18 @@
+<?php
+  // =============
+  // while loops
+  // =============
+  // *** Same usage as JavaScript ***
+  
+  // While loops run some code infinitely while some condition remains true.
+
+  // To stop the code in a while loop from running, you need to write some code to make
+  // the initial condition false.
+  
+  $counter = 0;
+
+  while ($counter < 10) {
+    $counter++;
+    echo $counter . "<br>";
+  }
+?>
