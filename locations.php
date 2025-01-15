15b2a074c0f3b91d2fad45852842ad78c8c65e17
Allan Viray <allang.v.dev@gmail.com>
2025-01-14 19:21:31 -0600

    Add html content and include header and footer in about.php and locations.php files

---
+++ locations.php
@@ -0,0 +1,18 @@
+<?php
+  include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Locations</title>
+</head>
+<body>
+  <p>This is the Locations page.</p>
+  <p>Add information about locations on this page.</p>
+</body>
+</html>
+<?php
+  include("footer.html");
+?>
