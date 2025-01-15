a429bb3cfe90cc90bf6d3a50081790c568202327
Allan Viray <allang.v.dev@gmail.com>
2025-01-14 18:52:35 -0600

    Create include.php file and content

---
+++ include.php
@@ -0,0 +1,21 @@
+<?php
+  // ========================
+  // The include() function
+  // ========================
+  // The include() function copies the content of a file (ie a php/html/text file) and includes
+  // it in your php file.
+  
+  // **********
+  // Benefits
+  // **********
+  // Sections of your website can become reusable.
+  // Changes only need to be made in one place.
+
+  // **********
+  // Example
+  // **********
+  // You can create HTML files for a header and a footer.
+  // Then, in each page of your website, you can include that same header and footer.
+  // Therefore, these files are reusable.
+
+?>
