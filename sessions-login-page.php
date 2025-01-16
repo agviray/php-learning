e060b86d124d8b04a9a424fe188ce2b84cc00144
Allan Viray <allang.v.dev@gmail.com>
2025-01-15 20:27:16 -0600

    Add form to login page and basic validation

--- sessions-login-page.php
+++ sessions-login-page.php
@@ -22,20 +22,30 @@
 <body>
   <h1>This is the Sessions Login Page</h1>
   <a href="sessions-home-page.php">This goes to the Sessions Home Page</a>
+  <br><br>
+  <form action="sessions-login-page.php" method="post">
+    <span style="display: block;"><label>Username</label></span>
+    <span style="display: block;"><input type="text" name="username"></span>
+    <span style="display: block;">Password</span>
+    <span style="display: block;"><input type="password" name="password"></span>
+    <span style="display: block;"><input type="submit" name="login" value="login"></span>
+  </form>
 </body>
 </html>
 <?php
   // 3) Create session key=>value pairs within the $_SESSION superglobal variable.
 
-  // Here, you stored a username and password in the $_SESSION superglobal variable.
+  // Here, you stored a username and password in the $_SESSION superglobal variable, after checking if a user entered them in the form.
   // You can access their values using their keys.
+  if (isset($_POST["login"])) {
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+      $_SESSION["username"] = $_POST["username"];
+      $_SESSION["password"] = $_POST["password"];
 
-  $_SESSION["username"] = "agv123";
-  $_SESSION["password"] = "password123";
-
-  echo "<br>";
-  echo $_SESSION["username"] . "<br>";
-  echo $_SESSION["password"] . "<br>";
+    } else {
+      echo "Please check your credentials. <br>";
+    }
+  }
 
   // 4) You now want to be able to access these values in the sessions-home-page.php 
 ?>
