bdf88a4dfb5852f707ced6435223fd072d893f2c
Allan Viray <allang.v.dev@gmail.com>
2025-01-15 19:48:20 -0600

    Create sessions.php file and content

---
+++ sessions.php
@@ -0,0 +1,16 @@
+<?php
+  // =================
+  // Sessions in PHP
+  // =================
+  // A session in PHP, is a superglobal variable that's used to store info on a user,
+  // to be used across multiple pages.
+  // Once a session is created, a user is assigned a session ID.
+
+  // Example: login credentials
+  // Once a user logs into a site, such as Facebook, the user can visit different pages on
+  // that site and stay logged in.
+
+  // See associated files:
+  // sessions-home-page.php
+  // sessions-login-page.php
+?>
